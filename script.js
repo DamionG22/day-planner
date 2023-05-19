0c95d1ca4517b0cb569c11ec2e076453e92bdb3b
blue butter <damiongallarza@gmail.com>
2023-05-18 19:48:14 -0700

    final updates for code, Javascript code added for each save button to save input to local storage.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+// Converts HTML elements to variable in Javascript
 var todaysDate = document.getElementById("currentDay");
-var saveButton = document.getElementById("save-button8");
-var saveButton = document.getElementById("save-button9");
-var saveButton = document.getElementById("save-button10");
-var saveButton = document.getElementById("save-button11");
-var saveButton = document.getElementById("save-button12");
-var saveButton = document.getElementById("save-button13");
-var saveButton = document.getElementById("save-button14");
-var saveButton = document.getElementById("save-button15");
-var saveButton = document.getElementById("save-button16");
-var saveButton = document.getElementById("save-button17");
-var saveButton = document.getElementById("save-button18");
-var hour8Box = document.getElementById("hour-8");
+var saveButton9 = document.getElementById("save-button9");
+var saveButton10 = document.getElementById("save-button10");
+var saveButton11 = document.getElementById("save-button11");
+var saveButton12 = document.getElementById("save-button12");
+var saveButton13 = document.getElementById("save-button13");
+var saveButton14 = document.getElementById("save-button14");
+var saveButton15 = document.getElementById("save-button15");
+var saveButton16 = document.getElementById("save-button16");
+var saveButton17 = document.getElementById("save-button17");
 var hour9Box = document.getElementById("hour-9");
 var hour10Box = document.getElementById("hour-10");
 var hour11Box = document.getElementById("hour-11");
@@ -20,8 +18,6 @@ var hour14Box = document.getElementById("hour-14");
 var hour15Box = document.getElementById("hour-15");
 var hour16Box = document.getElementById("hour-16");
 var hour17Box = document.getElementById("hour-17");
-var hour18Box = document.getElementById("hour-18");
-var textBox8 = document.getElementById("textBox8");
 var textBox9 = document.getElementById("textBox9");
 var textBox10 = document.getElementById("textBox10");
 var textBox11 = document.getElementById("textBox11");
@@ -31,20 +27,18 @@ var textBox14 = document.getElementById("textBox14");
 var textBox15 = document.getElementById("textBox15");
 var textBox16 = document.getElementById("textBox16");
 var textBox17 = document.getElementById("textBox17");
-var textBox18 = document.getElementById("textBox18");
 
 
-textBox8.value = localStorage.getItem("typedText");
-textBox9.value = localStorage.getItem("typedText");
-textBox10.value = localStorage.getItem("typedText");
-textBox11.value = localStorage.getItem("typedText");
-textBox12.value = localStorage.getItem("typedText");
-textBox13.value = localStorage.getItem("typedText");
-textBox14.value = localStorage.getItem("typedText");
-textBox15.value = localStorage.getItem("typedText");
-textBox16.value = localStorage.getItem("typedText");
-textBox17.value = localStorage.getItem("typedText");
-textBox18.value = localStorage.getItem("typedText");
+// CHecks local storage for each hour block to see if they have saved text.
+textBox9.value = localStorage.getItem("typedText9");
+textBox10.value = localStorage.getItem("typedText10");
+textBox11.value = localStorage.getItem("typedText11");
+textBox12.value = localStorage.getItem("typedText12");
+textBox13.value = localStorage.getItem("typedText13");
+textBox14.value = localStorage.getItem("typedText14");
+textBox15.value = localStorage.getItem("typedText15");
+textBox16.value = localStorage.getItem("typedText16");
+textBox17.value = localStorage.getItem("typedText17");
 
 
 var currentDay = document.getElementById("currentDay");
@@ -59,68 +53,109 @@ updateDateTime();
 // sets time to update by 1 second
 setInterval(updateDateTime, 1000);
 
+// Function to update hour block color based on time of day
+function updateClassBasedOnTime() {
+  var currentTime = new Date();
+  var currentHour = currentTime.getHours();
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-  
-});
-
-// $("#textBox8").siblings()
-
-
-// saveButton.addEventListener("click", function (event){
-//   event.preventDefault
-//   const typedText = textBox8.value;
-//   console.log(textBox8.value);
-//   // textBox8.value = localStorage.getItem("typedText");
-//    localStorage.setItem("typedText", typedText);
-//   console.log(typedText);
-// }
-// );
-
-
-for (var i = 8; i <= 18; i++) {
-  var textBox = $("#text-box" + i);
-  var saveButton = $("#save-button" + i);
-
-  // Add event listener to each save button
-  saveButton.on("click", function(event) {
-    event.preventDefault();
-    
-    var typedText = textBox.value();
-    console.log(textBox.value());
-    localStorage.setItem("typedText", typedText);
-    console.log(typedText);
-  });
+  if (currentHour >= 15 && currentHour < 23) {
+    hourBox14.classList.add("past");
+    hourBox14.classList.remove("present","future");
+  } else if (currentHour === 14) {
+    hourBox14.classList.add("present");
+    hourBox14.classList.remove("past","future");
+  } else {
+    hourBox14.classList.add("future");
+    hourBox14.classList.remove("present","past");
+  }
 }
 
-// textBox8.value = localStorage.getItem("typedText");
-// textBox8.addEventListener("input", function(event) {
-//   const typedText = event.target.value;
-//   textBox8.value = localStorage.getItem("typedText");
-//    localStorage.setItem('typedText', typedText); 
+// Initial class update
+updateClassBasedOnTime();
 
-//     console.log(typedText);
+// Update class every minute
+setInterval(updateClassBasedOnTime, 60000);
 
-// })
+
+// Event Listeners for each save button to save text to local storage.
+saveButton9.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText9 = textBox9.value;
+  console.log(textBox9.value);
+   localStorage.setItem("typedText9", typedText9);
+  console.log(typedText);
+}
+);
+
+saveButton10.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText10 = textBox10.value;
+  console.log(textBox10.value);
+   localStorage.setItem("typedText10", typedText10);
+  console.log(typedText);
+}
+);
+
+saveButton11.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText11 = textBox11.value;
+  console.log(textBox11.value);
+   localStorage.setItem("typedText11", typedText11);
+  console.log(typedText);
+}
+);
+
+saveButton12.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText12 = textBox12.value;
+  console.log(textBox12.value);
+   localStorage.setItem("typedText12", typedText12);
+  console.log(typedText);
+}
+);
+
+saveButton13.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText13 = textBox13.value;
+  console.log(textBox11.value);
+   localStorage.setItem("typedText13", typedText13);
+  console.log(typedText);
+}
+);
+
+saveButton14.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText14 = textBox14.value;
+  console.log(textBox14.value);
+   localStorage.setItem("typedText14", typedText14);
+  console.log(typedText);
+}
+);
+
+saveButton15.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText15 = textBox15.value;
+  console.log(textBox15.value);
+   localStorage.setItem("typedText15", typedText15);
+  console.log(typedText);
+}
+);
+
+saveButton16.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText16 = textBox16.value;
+  console.log(textBox16.value);
+   localStorage.setItem("typedText16", typedText16);
+  console.log(typedText);
+}
+);
+
+saveButton17.addEventListener("click", function (event){
+  event.preventDefault
+  const typedText17 = textBox17.value;
+  console.log(textBox16.value);
+   localStorage.setItem("typedText17", typedText17);
+  console.log(typedText);
+}
+);
+
